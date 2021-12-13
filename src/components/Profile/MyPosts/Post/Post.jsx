318fd3bd8302ed0119a4ad132eb12a52d7a9c561
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2021-12-13 22:08:04 +0300

    few fixes

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,7 @@ export const Post = (props) => {
 
     return (
         <div className={classes.item}>
-            <img src={"https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg"}/>
+            <img src={"https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg"} alt={}/>
             {props.message}
             <div>
                 <span>Likes:</span>
--- src/redux/state.js
+++ src/redux/state.js
@@ -16,28 +16,28 @@ let store = {
         },
         messagePage: {
             dialogsData: [
-                {id: 1, name: "Dim", avatar: <img src={"https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"}/>},
-                {id: 2, name: "Andrew", avatar: <img src={"https://cdn.freelance.ru/images/att/1324133_900_600.png"}/>},
+                {id: 1, name: "Dim", avatar: <img src={"https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"} alt={}/>},
+                {id: 2, name: "Andrew", avatar: <img src={"https://cdn.freelance.ru/images/att/1324133_900_600.png"} alt={}/>},
                 {
                     id: 3,
                     name: "John",
-                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504627134515650.jpg"}/>
+                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504627134515650.jpg"} alt={}/>
                 },
                 {
                     id: 4,
                     name: "Silver",
-                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504650176973363.webp"}/>
+                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504650176973363.webp"} alt={}/>
                 },
                 {
                     id: 5,
                     name: "Pam",
-                    avatar: <img src={"https://cs13.pikabu.ru/post_img/2020/04/17/11/1587146536174888206.webp"}/>
+                    avatar: <img src={"https://cs13.pikabu.ru/post_img/2020/04/17/11/1587146536174888206.webp"} alt={}/>
                 },
                 {
                     id: 6,
                     name: "Gucci",
                     avatar: <img
-                        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZXiLIwZ4MJ4wim5PJAEv-8pjZR6omqL6qFw&usqp=CAU"}/>
+                        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZXiLIwZ4MJ4wim5PJAEv-8pjZR6omqL6qFw&usqp=CAU"} alt={}/>
                 },
             ],
 
