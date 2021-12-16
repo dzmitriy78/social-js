17479f85469b18fbba6fb0ee8ff58103d09b7aeb
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2021-12-16 22:08:44 +0300

    fixed few problems

--- src/redux/state.js
+++ src/redux/state.js
@@ -16,28 +16,28 @@ let store = {
         },
         messagePage: {
             dialogsData: [
-                {id: 1, name: "Dim", avatar: <img src={"https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"} alt={}/>},
-                {id: 2, name: "Andrew", avatar: <img src={"https://cdn.freelance.ru/images/att/1324133_900_600.png"} alt={}/>},
+                {id: 1, name: "Dim", avatar: <img src={"https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"} alt={1}/>},
+                {id: 2, name: "Andrew", avatar: <img src={"https://cdn.freelance.ru/images/att/1324133_900_600.png"} alt={2}/>},
                 {
                     id: 3,
                     name: "John",
-                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504627134515650.jpg"} alt={}/>
+                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504627134515650.jpg"} alt={3}/>
                 },
                 {
                     id: 4,
                     name: "Silver",
-                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504650176973363.webp"} alt={}/>
+                    avatar: <img src={"https://cs14.pikabu.ru/post_img/2021/05/08/12/1620504650176973363.webp"} alt={4}/>
                 },
                 {
                     id: 5,
                     name: "Pam",
-                    avatar: <img src={"https://cs13.pikabu.ru/post_img/2020/04/17/11/1587146536174888206.webp"} alt={}/>
+                    avatar: <img src={"https://cs13.pikabu.ru/post_img/2020/04/17/11/1587146536174888206.webp"} alt={5}/>
                 },
                 {
                     id: 6,
                     name: "Gucci",
                     avatar: <img
-                        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZXiLIwZ4MJ4wim5PJAEv-8pjZR6omqL6qFw&usqp=CAU"} alt={}/>
+                        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZXiLIwZ4MJ4wim5PJAEv-8pjZR6omqL6qFw&usqp=CAU"} alt={6}/>
                 },
             ],
 
