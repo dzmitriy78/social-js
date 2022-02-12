3a39b45ba39fb9ae66fef5304732fbcf5bcbbefe
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2022-02-12 15:00:52 +0300

    added login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import classes from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
-export const Header = ()=> {
+export const Header = (props) => {
     return (
         <header className={classes.header}>
-            <img src="https://previews.123rf.com/images/wavy28/wavy281605/wavy28160500900/59350543-hf-logo.jpg" alt={""}/>
+            <img src="https://previews.123rf.com/images/wavy28/wavy281605/wavy28160500900/59350543-hf-logo.jpg"
+                 alt={""}/>
+            <div className={classes.loginBlock}>
+                {props.isAuth? props.login
+                    : <NavLink to={"/login"}>Login</NavLink>
+                }
+            </div>
         </header>
     )
 }
