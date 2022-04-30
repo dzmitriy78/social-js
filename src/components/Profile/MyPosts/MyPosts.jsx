c9550c2646c2eef255f740217b0d01266c8df369
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2022-04-30 11:17:32 +0300

    add Formik myPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,33 +1,50 @@
 import React from "react";
 import classes from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
+import {Field, Form, Formik} from "formik";
 
 export const MyPosts = (props) => {
 
     let postElement = props.postData
         .map((p,i) => <Post key = {i} id={p.id} message={p.message} likes={p.likeCount}/>)
 
-    let onAddPost = () => {
+    let onAddPost = (values) => {
+        props.onPostChange(values.newPostText)
         props.addPost();
-    }
-
-    let onPostChange = (e) => {
-        let text = e.target.value;
-        props.onPostChange(text);
     }
 
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <div>
+                {<Formik
+                    initialValues={{
+                        newPostText: props.newPostText
+                    }}
+                    onSubmit={ (values, { resetForm }) => {
+                        onAddPost(values)
+                        resetForm( {values: ''})
+                    }}
+                  >
+                    {() => (
+                        <Form>
+                            <div>
+                                <Field type={'text'} name={'newPostText'} placeholder={'write a post'}/>
+                            </div>
+                            <button type={'submit'}>Add post</button>
+                        </Form>
+                    )}
+                </Formik>}
+
+
+                {/*<div>
                     <textarea onChange={onPostChange} value={props.newPostText}/>
                 </div>
                 <div>
                     <button disabled={props.newPostText === ""}
                             onClick={onAddPost}>Add post
                     </button>
-                </div>
+                </div>*/}
                 <div className={classes.posts}>
                     {postElement}
                 </div>
